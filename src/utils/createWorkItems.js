4393df0ae788f653d0aca9ff624af0be84ff2aa7
Christopher Clemons <chris.clemons85@gmail.com>
2019-11-30 23:34:39 -0800

    fix typo

--- src/utils/createWorkItems.js
+++ src/utils/createWorkItems.js
@@ -6,16 +6,16 @@ export default function createWorkItems(n, totalTime) {
 
   for (let i = 0; i < n; i++) {
     let date = new Date();
-    let created = date.setHours(date.getHours() - 4 * i);
-    let start = date.setHours(date.getHours() - 4 * i);
-    let end = date.setHours(date.getHours() - 4 * i) + totalTime / n;
+    // let created = date.setHours(date.getHours() - 4 * i);
+    // let start = date.setHours(date.getHours() - 4 * i);
+    // let end = date.setHours(date.getHours() - 4 * i) + totalTime / n;
 
     workItems.push({
       id: uuid(),
-      created,
-      start,
-      end,
-      duratio: totalTime / n,
+      created: date.setHours(date.getHours() - 4 * i),
+      start: date.setHours(date.getHours() - 4 * i),
+      end: date.setHours(date.getHours() - 4 * i) + totalTime / n,
+      duration: totalTime / n,
       notes: 'Did some work.'
     });
   }
