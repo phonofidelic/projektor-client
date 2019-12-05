961d1867c971baf8f97fc4caed357d234822198c
Christopher Clemons <chris.clemons85@gmail.com>
2019-12-05 14:31:29 -0800

    add project item menu with delete action UI

---
+++ src/components/ProjectGridItem.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Delete from '@material-ui/icons/Delete';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const StyledGridItem = styled(Grid)`
+  padding: 20px;
+  // border: 1px solid red;
+  text-decoration: none;
+`;
+
+const CardHeader = styled.div`
+  display: flex;
+  padding: 16px;
+`;
+
+const ProjectInfoContainer = styled.div`
+  // display: flex;
+  // justify-content: space-around;
+`;
+
+export default function ProjectGridItem(props) {
+  const { project } = props;
+
+  const [anchorEl, setAnchor] = useState(null);
+  const handleMenuClick = e => {
+    setAnchor(e.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchor(null);
+  };
+
+  return (
+    <StyledGridItem key={project._id} item xs={12} sm={6} md={4}>
+      <Card>
+        <CardHeader>
+          <Typography
+            style={{ flexGrow: 1 }}
+            gutterBottom
+            variant="h5"
+            component="h2"
+          >
+            {project.title}
+          </Typography>
+          <div>
+            <IconButton onClick={handleMenuClick}>
+              <MoreVertIcon />
+            </IconButton>
+            <Menu
+              id={`project-options-menu_${project._id}`}
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleCloseMenu}
+            >
+              <MenuItem>
+                <ListItemIcon>
+                  <Delete />
+                </ListItemIcon>
+                Delete
+              </MenuItem>
+            </Menu>
+          </div>
+        </CardHeader>
+        <CardActionArea component={Link} to={`projects/${project._id}`}>
+          <CardContent>
+            <ProjectInfoContainer>
+              <Typography variant="overline">Start Date:</Typography>{' '}
+              {project.startDate
+                ? moment(project.startDate).format('ddd, MMM Do YYYY')
+                : 'TBD'}
+              <br />
+              <Typography variant="overline">Dealine:</Typography>{' '}
+              {project.deadline
+                ? moment(project.deadline).format('ddd, MMM Do YYYY')
+                : 'open'}
+            </ProjectInfoContainer>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              style={{ height: 100, overflowY: 'auto' }}
+            >
+              {project.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <div style={{ height: 20 }}>
+          {project.budgetedTime && (
+            <LinearProgress
+              value={(project.timeUsed / (project.budgetedTime * 3.6e6)) * 100}
+              variant="determinate"
+              style={{ height: 20 }}
+            />
+          )}
+        </div>
+      </Card>
+    </StyledGridItem>
+  );
+}
--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
-import mockProjects from 'utils/mockProjects';
+
+import ProjectGridItem from 'components/ProjectGridItem';
 
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -19,72 +13,13 @@ const Container = styled.div`
   text-align: left;
 `;
 
-const StyledGridItem = styled(Grid)`
-  padding: 20px;
-  // border: 1px solid red;
-  text-decoration: none;
-`;
-
-const ProjectInfoContainer = styled.div`
-  // display: flex;
-  // justify-content: space-around;
-`;
-
 export default function ProjectGrid(props) {
   const { projects } = props;
   return (
     <Container>
       <Grid container>
         {projects.map(project => (
-          <StyledGridItem
-            key={project._id}
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            component={Link}
-            to={`projects/${project._id}`}
-          >
-            <Card>
-              <CardActionArea>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {project.title}
-                  </Typography>
-                  <ProjectInfoContainer>
-                    <Typography variant="overline">Start Date:</Typography>{' '}
-                    {project.startDate
-                      ? moment(project.startDate).format('ddd, MMM Do YYYY')
-                      : 'TBD'}
-                    <br />
-                    <Typography variant="overline">Dealine:</Typography>{' '}
-                    {project.deadline
-                      ? moment(project.deadline).format('ddd, MMM Do YYYY')
-                      : 'open'}
-                  </ProjectInfoContainer>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                    style={{ height: 100, overflowY: 'auto' }}
-                  >
-                    {project.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <div style={{ height: 20 }}>
-                {project.budgetedTime && (
-                  <LinearProgress
-                    value={
-                      (project.timeUsed / (project.budgetedTime * 3.6e6)) * 100
-                    }
-                    variant="determinate"
-                    style={{ height: 20 }}
-                  />
-                )}
-              </div>
-            </Card>
-          </StyledGridItem>
+          <ProjectGridItem project={project} />
         ))}
       </Grid>
     </Container>
