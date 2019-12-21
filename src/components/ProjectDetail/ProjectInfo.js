d35d30fb4541645e3210ddab66c2193d3edc20c8
Christopher Clemons <chris.clemons85@gmail.com>
2019-12-21 01:21:36 -0800

    create ProjectInfo component

---
+++ src/components/ProjectDetail/ProjectInfo.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import moment from 'moment';
+// eslint-disable-next-line
+import momentDurationFormatSetup from 'moment-duration-format';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const Container = styled(Grid)`
+  padding: 18px;
+  max-height: 300px;
+  overflow-y: auto;
+  white-space: pre-wrap;
+`;
+
+export default function ProjectInfo(props) {
+  const { project } = props;
+
+  return (
+    <Container item xs={12} sm={6}>
+      <div>
+        <Typography variant="overline">Client:</Typography> {project.client}
+      </div>
+      <div>
+        <Typography variant="overline">Start Date:</Typography>{' '}
+        {project.startDate
+          ? moment(project.startDate).format('ddd, MMM Do YYYY')
+          : 'TBD'}
+      </div>
+      <div>
+        <Typography variant="overline">Deadline:</Typography>{' '}
+        {project.deadline
+          ? moment(project.deadline).format('ddd, MMM Do YYYY')
+          : 'open'}
+      </div>
+      <div>
+        <Typography variant="overline">Time Used:</Typography>{' '}
+        {moment
+          .duration(project.timeUsed, 'ms')
+          .format('hh:mm:ss', { trim: false })}
+      </div>
+      <div>
+        <Typography variant="overline">Budgeted Time:</Typography>{' '}
+        {project.budgetedTime}h
+      </div>
+      <div>
+        <LinearProgress
+          value={(project.timeUsed / (project.budgetedTime * 3.6e6)) * 100}
+          variant="determinate"
+          style={{ height: 20 }}
+        />
+      </div>
+    </Container>
+  );
+}
--- src/components/ProjectDetail/index.js
+++ src/components/ProjectDetail/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import moment from 'moment';
-// eslint-disable-next-line
-import momentDurationFormatSetup from 'moment-duration-format';
 
+import ProjectInfo from 'components/ProjectDetail/ProjectInfo';
 import WorkSection from 'components/ProjectDetail/WorkSection';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import LinearProgress from '@material-ui/core/LinearProgress';
 
 const InfoContainer = styled(Grid)`
   margin-top: 40px;
@@ -32,40 +29,7 @@ export default function ProjectDetail(props) {
         <Info item xs={12} sm={6}>
           <Typography>{project.description}</Typography>
         </Info>
-        <Info item xs={12} sm={6}>
-          <div>
-            <Typography variant="overline">Client:</Typography> {project.client}
-          </div>
-          <div>
-            <Typography variant="overline">Start Date:</Typography>{' '}
-            {project.startDate
-              ? moment(project.startDate).format('ddd, MMM Do YYYY')
-              : 'TBD'}
-          </div>
-          <div>
-            <Typography variant="overline">Deadline:</Typography>{' '}
-            {project.deadline
-              ? moment(project.deadline).format('ddd, MMM Do YYYY')
-              : 'open'}
-          </div>
-          <div>
-            <Typography variant="overline">Time Used:</Typography>{' '}
-            {moment
-              .duration(project.timeUsed, 'ms')
-              .format('hh:mm:ss', { trim: false })}
-          </div>
-          <div>
-            <Typography variant="overline">Budgeted Time:</Typography>{' '}
-            {project.budgetedTime}h
-          </div>
-          <div>
-            <LinearProgress
-              value={(project.timeUsed / (project.budgetedTime * 3.6e6)) * 100}
-              variant="determinate"
-              style={{ height: 20 }}
-            />
-          </div>
-        </Info>
+        <ProjectInfo project={project} />
       </InfoContainer>
       <WorkSection project={project} createWork={createWork} />
     </div>
