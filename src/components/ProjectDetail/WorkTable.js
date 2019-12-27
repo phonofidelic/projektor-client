3b8767393cd1072ed767b41465684625e52317df
Christopher Clemons <chris.clemons85@gmail.com>
2019-12-27 13:24:44 -0800

    implement context menu

--- src/components/ProjectDetail/WorkTable.js
+++ src/components/ProjectDetail/WorkTable.js
@@ -1,7 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StringContext } from 'strings';
 import moment from 'moment';
 
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import Tooltip from '@material-ui/core/Tooltip';
@@ -12,14 +17,85 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import PostAddIcon from '@material-ui/icons/PostAdd';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
+function ContextMenu(props) {
+  const {
+    open,
+    contextPos,
+    workItem,
+    handleCloseContextMenu,
+    hadleOpenNote
+  } = props;
+
+  const selectEdit = () => {
+    hadleOpenNote(workItem);
+    handleCloseContextMenu();
+  };
+
+  return (
+    <Menu
+      open={open}
+      style={{ padding: '0px', position: 'fixed', transformOrigin: 0 }}
+      anchorEl={document.body}
+      anchorOrigin={{
+        vertical: contextPos.y + window.scrollY || 0,
+        horizontal: contextPos.x || 0
+      }}
+      getContentAnchorEl={null}
+      onClose={handleCloseContextMenu}
+    >
+      <MenuItem key="add-note" button component={ListItem} onClick={selectEdit}>
+        <ListItemIcon>
+          <EditIcon />
+        </ListItemIcon>
+        <ListItemText>Edit</ListItemText>
+      </MenuItem>
+      <MenuItem key="delete-work" button component={ListItem}>
+        <ListItemIcon>
+          <DeleteIcon />
+        </ListItemIcon>
+        <ListItemText>Delete</ListItemText>
+      </MenuItem>
+    </Menu>
+  );
+}
 
 export default function WorkTable(props) {
   const { project, hadleOpenNote } = props;
   const strings = useContext(StringContext);
   const currentLocaleData = moment.localeData();
+  const [selectedWork, setSelectedWork] = useState({ _id: null });
+  const [contextOpen, setContextMenuOpen] = useState(false);
+  const [contextPos, setContextPos] = useState({});
+
+  const handleSelectWork = work => {
+    setSelectedWork(work);
+  };
+
+  const handleOpenContextMenu = (e, workItem) => {
+    console.log('openContextMenu, e:', e);
+    e.preventDefault();
+    setSelectedWork(workItem);
+    setContextPos({ x: e.clientX, y: e.clientY });
+    setContextMenuOpen(true);
+  };
+
+  const handleCloseContextMenu = () => {
+    setContextPos({});
+    setContextMenuOpen(false);
+  };
 
   return (
     <Paper style={{ margin: 18 }}>
+      <ContextMenu
+        open={contextOpen}
+        contextPos={contextPos}
+        workItem={selectedWork}
+        hadleOpenNote={hadleOpenNote}
+        handleCloseContextMenu={handleCloseContextMenu}
+      />
       <Table size="small">
         <TableHead>
           <TableRow>
@@ -28,12 +104,18 @@ export default function WorkTable(props) {
             <TableCell>{strings.lbl__work_tbl_end_time}</TableCell>
             <TableCell>{strings.lbl__work_tble_duration}</TableCell>
             <TableCell>{strings.lbl__work_tble_notes}</TableCell>
-            <TableCell></TableCell>
+            {/* <TableCell style={{ width: 71 }}></TableCell> */}
           </TableRow>
         </TableHead>
         <TableBody>
           {project.work.map(workItem => (
-            <TableRow key={workItem._id} hover>
+            <TableRow
+              key={workItem._id}
+              hover
+              selected={workItem._id === selectedWork._id}
+              onClick={() => handleSelectWork(workItem)}
+              onContextMenu={e => handleOpenContextMenu(e, workItem)}
+            >
               <TableCell>
                 <Typography>
                   {moment(workItem.date).format(
@@ -66,29 +148,53 @@ export default function WorkTable(props) {
               </TableCell>
               <TableCell>
                 {workItem.notes ? (
-                  <Typography noWrap style={{ maxWidth: 150 }}>
+                  <Typography
+                    noWrap
+                    style={{ maxWidth: 150, cursor: 'pointer' }}
+                    onClick={() => hadleOpenNote(workItem)}
+                  >
                     {workItem.notes}
                   </Typography>
                 ) : (
-                  <Typography variant="caption" style={{ margin: 10 }}>
+                  <Typography
+                    variant="caption"
+                    style={{ margin: 10, cursor: 'pointer' }}
+                    onClick={() => hadleOpenNote(workItem)}
+                  >
                     {strings.msg__default_empty_notes}
                   </Typography>
                 )}
               </TableCell>
-              <TableCell>
-                <Tooltip
-                  title={strings.hnt__add_note}
-                  placement="top-start"
-                  enterDelay={400}
-                >
-                  <IconButton
-                    size="small"
-                    onClick={() => hadleOpenNote(workItem)}
-                  >
-                    <PostAddIcon />
-                  </IconButton>
-                </Tooltip>
-              </TableCell>
+              {/* <TableCell style={{ padding: 0 }}>
+                {workItem._id === selectedWork._id && (
+                  <div style={{ display: 'flex' }}>
+                    <Tooltip
+                      title={strings.hnt__add_note}
+                      placement="top-start"
+                      enterDelay={400}
+                    >
+                      <IconButton
+                        size="small"
+                        onClick={() => hadleOpenNote(workItem)}
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip
+                      title={strings.hnt__add_note}
+                      placement="top-start"
+                      enterDelay={400}
+                    >
+                      <IconButton
+                        size="small"
+                        onClick={() => hadleOpenNote(workItem)}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </div>
+                )}
+              </TableCell> */}
             </TableRow>
           ))}
         </TableBody>
