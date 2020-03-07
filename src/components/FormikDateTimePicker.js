ce5f9d1743ee208b93dd77a60a838a6dcb0d0593
Christopher Clemons <chris.clemons85@gmail.com>
2020-03-07 03:35:33 -0800

    show toolbar in KeyboardDateTimePicker

--- src/components/FormikDateTimePicker.js
+++ src/components/FormikDateTimePicker.js
@@ -1,9 +1,22 @@
 import React, { useContext } from 'react';
 import { StringContext } from 'strings';
-import {
-  KeyboardDateTimePicker,
-  KeyboardTimePicker
-} from '@material-ui/pickers';
+import { KeyboardDateTimePicker } from '@material-ui/pickers';
+
+const isAMPM = () => {
+  var date = new Date(Date.UTC(2012, 11, 12, 3, 0, 0));
+  var dateString = date.toLocaleTimeString();
+
+  //apparently toLocaleTimeString() has a bug in Chrome. toString() however returns 12/24 hour formats. If one of two contains AM/PM execute 12 hour coding.
+  if (dateString.match(/am|pm/i) || date.toString().match(/am|pm/i)) {
+    //12 hour clock
+    console.log('12 hour');
+    return true;
+  } else {
+    //24 hour clock
+    console.log('24 hour');
+    return false;
+  }
+};
 
 export default function FormikDateTimePicker({ field, form, ...other }) {
   // const { datetime, handleDateTimeError } = other;
@@ -12,10 +25,10 @@ export default function FormikDateTimePicker({ field, form, ...other }) {
 
   return (
     <KeyboardDateTimePicker
-      disableToolbar
+      // disableToolbar
       // format="MM/dd/yyyy"
       format="hh:mm a"
-      // ampm={false}
+      ampm={isAMPM()}
       name={field.name}
       // views={['date', 'hours', 'minutes']}
       value={field.value}
@@ -24,13 +37,9 @@ export default function FormikDateTimePicker({ field, form, ...other }) {
       KeyboardButtonProps={{
         'aria-label': 'change date'
       }}
+      // variant="inline"
       cancelLabel={strings.btn__cancel}
       onChange={date => form.setFieldValue(field.name, date, false)}
-      // onError={(error, value) => {
-      //   console.log('KeyboardDateTimePicker error:', error);
-      //   handleDateTimeError(error, value);
-      //   // return error;
-      // }}
       {...other}
     />
   );
