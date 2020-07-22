2ec9fed3a6104e22a8d578bfd04cad4d65e7c138
Christopher Clemons <chris.clemons85@gmail.com>
2020-07-22 00:06:53 -0700

    add UI for experimental task analysis, comment out ActiveWork component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,13 @@ import dashboardReducer from 'reducers/dashboard';
 import messageReducer from 'reducers/message';
 import { connectRouter } from 'connected-react-router';
 import { history } from 'config';
+// import { taskAnalysisReducer } from 'components/TaskAnalysis';
 
 export default combineReducers({
   projects: projectsReducer,
   user: userReducer,
   dashboard: dashboardReducer,
   message: messageReducer,
-  router: connectRouter(history)
+  router: connectRouter(history),
+  // taskAnalysis: taskAnalysisReducer,
 });
