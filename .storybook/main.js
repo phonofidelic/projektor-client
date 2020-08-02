c8a6e6aa236f8303ccbc829c940771ea9b55144f
Christopher Clemons <chris.clemons85@gmail.com>
2020-08-01 19:02:04 -0700

    add storybook docs add-on

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,11 @@ module.exports = {
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
   ],
 };
